2aa36144e4bf93e991545438457d57132366f6d6
Tom Overton <tom-overton@users.noreply.github.com>
2024-06-23 18:09:22 -0700

    EnOkuta (Octorok and its projectiles) OK and documented (#1641)
    * Match EnOkuta
    
    * Match data
    
    * Use the generated reloc
    
    * Delete extern stuff and format
    
    * Extract the DL
    
    * Fix lots of stuff
    
    * Copy a lot of names over from EnSyatekiOkuta
    
    * SFX enums
    
    * Lots more stuff
    
    * Constants and other stuff
    
    * Some more stuff I missed
    
    * Name more functions using OoT/SyatekiOkuta as reference
    
    * An easy function I missed
    
    * Some floats
    
    * Getter macros
    
    * Types enum
    
    * Name the cylinder inits a little clearer
    
    * Create bodyparts enum and name all data
    
    * Name all functions
    
    * Name `timer` struct var and all temps
    
    * Name the `jumpHeight` struct var
    
    * Create damage effect enum
    
    * Name the extracted DL
    
    * Name `numConsecutiveProjectiles`
    
    * Finish docs
    
    * Document XML
    
    * Clarify that this blue Octorok isn't the shooting gallery one
    
    * Respond to reviews
    
    * Stuff
    
    ---------
    
    Co-authored-by: Synray <31429825+Synray@users.noreply.github.com>
    Co-authored-by: Derek Hensley <hensley.derek58@gmail.com>

---
+++ mm/assets/overlays/ovl_En_Okuta/ovl_En_Okuta.h
@@ -0,0 +1,9 @@
+#ifndef OVERLAYS_OVL_EN_OKUTA_H
+#define OVERLAYS_OVL_EN_OKUTA_H 1
+
+#include "align_asset_macro.h"
+
+#define dgOctorokBlueMaterialDL "__OTR__objects/object_okuta/gOctorokBlueMaterialDL"
+static const ALIGN_ASSET(2) char gOctorokBlueMaterialDL[] = dgOctorokBlueMaterialDL;
+
+#endif // OVERLAYS_OVL_EN_OKUTA_H
