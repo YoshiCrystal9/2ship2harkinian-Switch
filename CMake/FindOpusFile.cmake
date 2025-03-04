5e41c0dc4ccf05bf5d31dfc61799dee594ef5f7f
YoshiCrystal9 <89108143+YoshiCrystal9@users.noreply.github.com>
2025-03-04 16:49:03 +0100

    Fixed custom audio and NRO name

--- CMake/FindOpusFile.cmake
+++ CMake/FindOpusFile.cmake
@@ -9,7 +9,7 @@
 # Search for the OpusFile header
 find_path(OPUSFILE_INCLUDE_DIR
     NAMES opusfile.h
-    PATHS /usr/include/opus /usr/local/include/opus /opt/local/include/opus
+    PATHS /usr/include/opus /usr/local/include/opus /opt/local/include/opus ${DEVKITPRO}/portlibs/switch/include/opus/
     DOC "Directory where opusfile.h is located"
 )
 
@@ -39,12 +39,23 @@ find_package_handle_standard_args(OpusFile
 )
 
 # Define an imported target if everything is found
-if (OPUSFILE_FOUND)
+if (OPUSFILE_FOUND AND CMAKE_SYSTEM_NAME MATCHES "NintendoSwitch")
+    add_library(OpusFile::opusfile INTERFACE IMPORTED)
+
+    set_target_properties(OpusFile::opusfile PROPERTIES
+            INTERFACE_INCLUDE_DIRECTORIES "${OPUSFILE_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${OPUSFILE_LIBRARY};${OPUS_LIBRARY};${OGG_LIBRARY}"
+    )
+
+    # Optionally expose the include and libraries separately
+    set(OPUSFILE_LIBRARIES ${OPUSFILE_LIBRARY} ${OPUS_LIBRARY} ${OGG_LIBRARY})
+    set(OPUSFILE_INCLUDE_DIRS ${OPUSFILE_INCLUDE_DIR})
+elseif(OPUSFILE_FOUND AND NOT CMAKE_SYSTEM_NAME MATCHES "NintendoSwitch")
     add_library(Opusfile::Opusfile INTERFACE IMPORTED)
 
     set_target_properties(Opusfile::Opusfile PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${OPUSFILE_INCLUDE_DIR}"
-        INTERFACE_LINK_LIBRARIES "${OPUSFILE_LIBRARY};${OPUS_LIBRARY};${OGG_LIBRARY}"
+            INTERFACE_INCLUDE_DIRECTORIES "${OPUSFILE_INCLUDE_DIR}"
+            INTERFACE_LINK_LIBRARIES "${OPUSFILE_LIBRARY};${OPUS_LIBRARY};${OGG_LIBRARY}"
     )
 
     # Optionally expose the include and libraries separately
