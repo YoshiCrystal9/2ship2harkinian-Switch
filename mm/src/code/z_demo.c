ec47ad847bf1331d732e1801ce2bcd6d7b24e2b1
Eblo <7004497+Eblo@users.noreply.github.com>
2025-07-05 12:54:47 -0400

    Replace erroneous CsCmdStartSeq with CsCmdStopSeq (#1812)

--- mm/src/code/z_demo.c
+++ mm/src/code/z_demo.c
@@ -1275,7 +1275,7 @@ void Cutscene_ProcessScript(PlayState* play, CutsceneContext* csCtx, u8* script)
 
                 for (j = 0; j < cmdEntries; j++) {
                     CutsceneCmd_StopSequence(play, csCtx, (CsCmdStopSeq*)script);
-                    script += sizeof(CsCmdStartSeq);
+                    script += sizeof(CsCmdStopSeq);
                 }
                 break;
 
