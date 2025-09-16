0109beff5d5f20f2653d4b436295e50fe96608e6
Eblo <7004497+Eblo@users.noreply.github.com>
2025-09-16 12:45:56 -0400

    Update collision flags in missed files

--- mm/2s2h/CustomItem/CustomItem.cpp
+++ mm/2s2h/CustomItem/CustomItem.cpp
@@ -12,7 +12,7 @@ extern "C" {
 // #region These were copied from z_en_item00.c
 static ColliderCylinderInit sCylinderInit = {
     {
-        COLTYPE_NONE,
+        COL_MATERIAL_NONE,
         AT_NONE,
         AC_ON | AT_TYPE_PLAYER,
         OC1_NONE,
@@ -20,11 +20,11 @@ static ColliderCylinderInit sCylinderInit = {
         COLSHAPE_CYLINDER,
     },
     {
-        ELEMTYPE_UNK0,
+        ELEM_MATERIAL_UNK0,
         { 0x00000000, 0x00, 0x00 },
         { 0x00000010, 0x00, 0x00 },
-        TOUCH_NONE | TOUCH_SFX_NORMAL,
-        BUMP_ON,
+        ATELEM_NONE | ATELEM_SFX_NORMAL,
+        ACELEM_ON,
         OCELEM_NONE,
     },
     { 10, 30, 0, { 0, 0, 0 } },
